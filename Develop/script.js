1e934772c517084d6e60079e38d6de8600f4b6a3
Dmitriy Aloyts <dmitriy@iinet.net.au>
2019-11-07 18:44:48 +1100

    working version of Day planner

---
+++ Develop/script.js
@@ -0,0 +1,173 @@
+$(document).ready(function () {
+
+    function renderDescriptions() {
+
+        desc09.value = localStorage.getItem("desc09");
+        desc10.value = localStorage.getItem("desc10");
+        desc11.value = localStorage.getItem("desc11");
+        desc12.value = localStorage.getItem("desc12");
+        desc13.value = localStorage.getItem("desc13");
+        desc14.value = localStorage.getItem("desc14");
+        desc15.value = localStorage.getItem("desc15");
+        desc16.value = localStorage.getItem("desc16");
+        desc17.value = localStorage.getItem("desc17");
+
+        $("#currentDay").text(moment().format('dddd, MMMM Do')); // Wednesday November 6th 
+        var curhour = moment().format('HH');
+
+        if (curhour < 09){
+            $("#desc09").attr("class","future col-8 description");
+            $("#desc10").attr("class","future col-8 description");
+            $("#desc11").attr("class","future col-8 description");
+            $("#desc12").attr("class","future col-8 description");
+            $("#desc13").attr("class","future col-8 description");
+            $("#desc14").attr("class","future col-8 description");
+            $("#desc15").attr("class","future col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 09){
+            $("#desc09").attr("class","present col-8 description");
+            $("#desc10").attr("class","future col-8 description");
+            $("#desc11").attr("class","future col-8 description");
+            $("#desc12").attr("class","future col-8 description");
+            $("#desc13").attr("class","future col-8 description");
+            $("#desc14").attr("class","future col-8 description");
+            $("#desc15").attr("class","future col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 10){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","present col-8 description");
+            $("#desc11").attr("class","future col-8 description");
+            $("#desc12").attr("class","future col-8 description");
+            $("#desc13").attr("class","future col-8 description");
+            $("#desc14").attr("class","future col-8 description");
+            $("#desc15").attr("class","future col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 11){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","present col-8 description");
+            $("#desc12").attr("class","future col-8 description");
+            $("#desc13").attr("class","future col-8 description");
+            $("#desc14").attr("class","future col-8 description");
+            $("#desc15").attr("class","future col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 12){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","past col-8 description");
+            $("#desc12").attr("class","present col-8 description");
+            $("#desc13").attr("class","future col-8 description");
+            $("#desc14").attr("class","future col-8 description");
+            $("#desc15").attr("class","future col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 13){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","past col-8 description");
+            $("#desc12").attr("class","past col-8 description");
+            $("#desc13").attr("class","present col-8 description");
+            $("#desc14").attr("class","future col-8 description");
+            $("#desc15").attr("class","future col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 14){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","past col-8 description");
+            $("#desc12").attr("class","past col-8 description");
+            $("#desc13").attr("class","past col-8 description");
+            $("#desc14").attr("class","present col-8 description");
+            $("#desc15").attr("class","future col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 15){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","past col-8 description");
+            $("#desc12").attr("class","past col-8 description");
+            $("#desc13").attr("class","past col-8 description");
+            $("#desc14").attr("class","past col-8 description");
+            $("#desc15").attr("class","present col-8 description");
+            $("#desc16").attr("class","future col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 16){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","past col-8 description");
+            $("#desc12").attr("class","past col-8 description");
+            $("#desc13").attr("class","past col-8 description");
+            $("#desc14").attr("class","past col-8 description");
+            $("#desc15").attr("class","past col-8 description");
+            $("#desc16").attr("class","present col-8 description");
+            $("#desc17").attr("class","future col-8 description");
+        }
+        if (curhour == 17){
+            console.log({curhour});
+
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","past col-8 description");
+            $("#desc12").attr("class","past col-8 description");
+            $("#desc13").attr("class","past col-8 description");
+            $("#desc14").attr("class","past col-8 description");
+            $("#desc15").attr("class","past col-8 description");
+            $("#desc16").attr("class","past col-8 description");
+            $("#desc17").attr("class","present col-8 description");
+        }
+        if (curhour > 17){
+            $("#desc09").attr("class","past col-8 description");
+            $("#desc10").attr("class","past col-8 description");
+            $("#desc11").attr("class","past col-8 description");
+            $("#desc12").attr("class","past col-8 description");
+            $("#desc13").attr("class","past col-8 description");
+            $("#desc14").attr("class","past col-8 description");
+            $("#desc15").attr("class","past col-8 description");
+            $("#desc16").attr("class","past col-8 description");
+            $("#desc17").attr("class","past col-8 description");
+        }
+    }
+
+    renderDescriptions();
+
+    $("#btn-09").on("click", function () {
+        localStorage.setItem("desc09", desc09.value);
+    });
+    $("#btn-10").on("click", function () {
+        localStorage.setItem("desc10", desc10.value);
+    });
+    $("#btn-11").on("click", function () {
+        localStorage.setItem("desc11", desc11.value);
+    });
+    $("#btn-12").on("click", function () {
+        localStorage.setItem("desc12", desc12.value);
+    });
+    $("#btn-13").on("click", function () {
+        localStorage.setItem("desc13", desc13.value);
+    });
+    $("#btn-14").on("click", function () {
+        localStorage.setItem("desc14", desc14.value);
+    });
+    $("#btn-15").on("click", function () {
+        localStorage.setItem("desc15", desc15.value);
+    });
+    $("#btn-16").on("click", function () {
+        localStorage.setItem("desc16", desc16.value);
+    });
+    $("#btn-17").on("click", function () {
+        localStorage.setItem("desc17", desc17.value);
+    });
+
+});
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Day-planner
+A simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+The app displays standard business hours (9 a.m. to 5 p.m.). Each time slot represents one hour and contain the following:
+
+* The time
+
+* A field to hold user input
+
+* A save button
+
+Clicking on the save button will store the time and user input in `localStorage`.
+
+Near the top of the calendar, the application should display the current day. Additionally, each hour is color coded to reflect whether the time slot is in the past, the present, or the future. This will change depending on the time of day.
+
+Used the [Moment.js](https://momentjs.com/) library to work with date and time.
